8e063b8e257f4b726b68ad4747201a06844e81eb
Abraham Kipkorir <masaikipkorir@gmail.com>
2019-06-12 07:37:59 +0300

    initial commit

---
+++ forms.py
@@ -0,0 +1,28 @@
+from django import forms
+
+
+class ContactForm(forms.Form):
+	fullname = forms.CharField(
+		widget=forms.TextInput(
+			attrs={
+			"class": "form-control", 
+			"placeholder": "Fullname"
+			}
+			)
+		)
+	email    = forms.EmailField(
+		widget=forms.EmailInput(
+			attrs={
+			"class": "form-control", 
+			"placeholder": "Email"
+			}
+			)
+		)
+	content  = forms.CharField(
+		widget=forms.Textarea(
+			attrs={
+			"class": "form-control", 
+			"placeholder": "Content"
+			}
+			)
+		)
